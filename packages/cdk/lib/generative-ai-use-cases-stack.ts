f1f4feebf95eaee63b27bd5c98192462745c6c7a
Toshiki Watanabe <toshikwa@gmail.com>
2023-11-02 18:46:43 +0900

    g

--- packages/cdk/lib/generative-ai-use-cases-stack.ts
+++ packages/cdk/lib/generative-ai-use-cases-stack.ts
@@ -8,9 +8,8 @@ export class GenerativeAiUseCasesStack extends Stack {
 
     process.env.overrideWarningsEnabled = 'false';
 
-    const ragEnabled: boolean = this.node.tryGetContext('ragEnabled') ?? false;
-    const selfSignUpEnabled: boolean =
-      this.node.tryGetContext('selfSignUpEnabled') ?? true;
+    const ragEnabled: boolean = this.node.tryGetContext('ragEnabled')!;
+    const selfSignUpEnabled: boolean = this.node.tryGetContext('selfSignUpEnabled')!;
 
     const auth = new Auth(this, 'Auth', {
       selfSignUpEnabled,
