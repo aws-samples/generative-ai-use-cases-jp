12d9661764501f702dda57890ab9f8ea0790c62f
Taichiro Suzuki <taichirs@amazon.co.jp>
2023-12-20 10:54:42 +0900

    fix

--- packages/web/src/hooks/useTyping.ts
+++ packages/web/src/hooks/useTyping.ts
@@ -5,25 +5,54 @@ const TYPING_DELAY = 10;
 const useTyping = (typing?: boolean) => {
   const [typingTextInput, setTypingTextInput] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
 
   useEffect(() => {
-    if (typing && currentIndex < typingTextInput.length) {
+    if (typing) {
+      // アニメーション開始時
+      if (!animating) {
+        setAnimating(true);
+      }
+    } else {
+      // アニメーション完了時 or 最初からアニメーションがないと指定された時
+      if (!animating) {
+        setCurrentIndex(0);
+      }
+    }
+  }, [typing, animating, setAnimating, setCurrentIndex]);
+
+  useEffect(() => {
+    if (animating && currentIndex <= typingTextInput.length + 1) {
       const timeout = setTimeout(() => {
-        setCurrentIndex(currentIndex + 1);
+        if (currentIndex < typingTextInput.length + 1) {
+          setCurrentIndex(currentIndex + 1);
+        } else {
+          // 入力文字列の末尾に追いついた時に typing が false になっていたらアニメーションを終了
+          if (!typing) {
+            setAnimating(false);
+          }
+        }
       }, TYPING_DELAY);
 
       return () => clearTimeout(timeout);
     }
-  }, [typingTextInput, currentIndex, typing]);
+  }, [
+    typingTextInput,
+    currentIndex,
+    animating,
+    typing,
+    setCurrentIndex,
+    setAnimating,
+  ]);
 
   const typingTextOutput = useMemo(() => {
-    if (typing) {
+    if (animating) {
       return typingTextInput.slice(0, currentIndex);
     } else {
-      // typing が false の場合は typingTextInput のまま返す
+      // animating が false の場合は typingTextInput をそのまま返す
       return typingTextInput;
     }
-  }, [typingTextInput, currentIndex, typing]);
+  }, [typingTextInput, currentIndex, animating]);
 
   return {
     setTypingTextInput,
