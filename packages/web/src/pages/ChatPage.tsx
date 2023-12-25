05093942091a2360a8d0b9cefc0b7aad8ca554fc
Taichiro Suzuki <taichirs@amazon.co.jp>
2023-12-25 12:08:46 +0900

    fix

--- packages/web/src/pages/ChatPage.tsx
+++ packages/web/src/pages/ChatPage.tsx
@@ -5,7 +5,11 @@ import useChat from '../hooks/useChat';
 import useConversation from '../hooks/useConversation';
 import ChatMessage from '../components/ChatMessage';
 import PromptList from '../components/PromptList';
+import Textarea from '../components/Textarea';
+import Button from '../components/Button';
+import ButtonIcon from '../components/ButtonIcon';
 import useScroll from '../hooks/useScroll';
+import { PiX, PiNotePencil, PiRobot } from 'react-icons/pi';
 import { create } from 'zustand';
 import { ReactComponent as BedrockIcon } from '../assets/bedrock.svg';
 import { ChatPageLocationState } from '../@types/navigate';
@@ -15,14 +19,17 @@ import { MODELS } from '../hooks/useModel';
 type StateType = {
   modelId: string;
   content: string;
+  inputSystemContext: string;
   setModelId: (c: string) => void;
   setContent: (c: string) => void;
+  setInputSystemContext: (c: string) => void;
 };
 
 const useChatPageState = create<StateType>((set) => {
   return {
     modelId: '',
     content: '',
+    inputSystemContext: '',
     setModelId: (s: string) => {
       set(() => ({
         modelId: s,
@@ -33,11 +40,23 @@ const useChatPageState = create<StateType>((set) => {
         content: s,
       }));
     },
+    setInputSystemContext: (s: string) => {
+      set(() => ({
+        inputSystemContext: s,
+      }));
+    },
   };
 });
 
 const ChatPage: React.FC = () => {
-  const { modelId, content, setModelId, setContent } = useChatPageState();
+  const {
+    modelId,
+    content,
+    inputSystemContext,
+    setModelId,
+    setContent,
+    setInputSystemContext,
+  } = useChatPageState();
   const { state, pathname } = useLocation() as Location<ChatPageLocationState>;
   const { chatId } = useParams();
 
@@ -49,6 +68,8 @@ const ChatPage: React.FC = () => {
     rawMessages,
     clear,
     postChat,
+    updateSystemContext,
+    getCurrentSystemContext,
   } = useChat(pathname, chatId);
   const { scrollToBottom, scrollToTop } = useScroll();
   const { getConversationTitle } = useConversation();
@@ -65,8 +86,12 @@ const ChatPage: React.FC = () => {
   useEffect(() => {
     if (state !== null) {
       setContent(state.content);
+
+      if (state.systemContext !== '') {
+        updateSystemContext(state.systemContext);
+      }
     }
-  }, [state, setContent]);
+  }, [state, setContent, updateSystemContext]);
 
   useEffect(() => {
     if (!modelId) {
@@ -109,6 +134,17 @@ const ChatPage: React.FC = () => {
     }
   }, [showSystemContext, rawMessages, messages]);
 
+  const [showSystemContextModal, setShowSystemContextModal] = useState(false);
+
+  const currentSystemContext = useMemo(() => {
+    const systemContext = getCurrentSystemContext();
+    return systemContext;
+  }, [getCurrentSystemContext]);
+
+  useEffect(() => {
+    setInputSystemContext(currentSystemContext);
+  }, [currentSystemContext, setInputSystemContext]);
+
   return (
     <>
       <div className={`${!isEmpty ? 'screen:pb-36' : ''} relative`}>
@@ -130,14 +166,88 @@ const ChatPage: React.FC = () => {
           </SelectField>
         </div>
 
-        {((isEmpty && !loadingMessages) || loadingMessages) && (
+        {loadingMessages && chatId && (
           <div className="relative flex h-[calc(100vh-9rem)] flex-col items-center justify-center">
-            <BedrockIcon
-              className={`fill-gray-400 ${
-                loadingMessages ? 'animate-pulse' : ''
-              }`}
-            />
+            <BedrockIcon className="animate-pulse fill-gray-400" />
           </div>
+        )}
+
+        {isEmpty && !loadingMessages && !chatId && !showSystemContextModal && (
+          <>
+            <div className="flex h-[calc(100vh-9rem)] flex-col items-center justify-center">
+              <div className="flex w-3/4 flex-col items-center justify-center text-gray-500 lg:w-1/2">
+                <div className="mb-2 flex items-center text-sm font-bold">
+                  <PiRobot className="mr-1 text-base" />
+                  システムコンテキスト
+                </div>
+                <div className="mb-4 max-h-64 w-full overflow-y-scroll whitespace-break-spaces text-center">
+                  {currentSystemContext}
+                </div>
+              </div>
+              <Button
+                className=""
+                onClick={() => {
+                  setShowSystemContextModal(true);
+                }}
+                outlined>
+                <PiNotePencil className="mr-1" />
+                変更
+              </Button>
+            </div>
+          </>
+        )}
+
+        {isEmpty && !loadingMessages && !chatId && showSystemContextModal && (
+          <>
+            <div
+              className="fixed left-0 top-0 z-[60] h-screen w-screen bg-gray-900/90"
+              onClick={() => {
+                setShowSystemContextModal(false);
+              }}></div>
+            <div className="h-[calc(100vh-9rem)] w-full">
+              <div className="absolute left-1/2 top-1/2 z-[70] flex w-3/4 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center border-2 bg-white p-4 shadow-lg lg:w-1/2">
+                <div className="mb-3 flex w-full items-center justify-between">
+                  <div></div>
+                  <div className="text-sm font-bold lg:text-lg">
+                    システムコンテキストの設定
+                  </div>
+                  <ButtonIcon
+                    onClick={() => {
+                      setShowSystemContextModal(false);
+                    }}>
+                    <PiX />
+                  </ButtonIcon>
+                </div>
+                <div className="w-full">
+                  <Textarea
+                    value={inputSystemContext}
+                    onChange={setInputSystemContext}
+                    rows={5}
+                  />
+                </div>
+                <div className="flex w-full justify-end">
+                  <Button
+                    className="mr-1"
+                    onClick={() => {
+                      clear();
+                      setInputSystemContext(currentSystemContext);
+                      setShowSystemContextModal(false);
+                    }}
+                    outlined>
+                    初期化
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      updateSystemContext(inputSystemContext);
+                      setShowSystemContextModal(false);
+                    }}
+                    disabled={inputSystemContext === currentSystemContext}>
+                    変更
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </>
         )}
 
         {!isEmpty && !loadingMessages && (
