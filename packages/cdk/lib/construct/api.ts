b22fe4c95a227ef3493b9ae9cd380b936af0627f
Kazuhito Go <gokazu@amazon.co.jp>
2024-08-28 11:29:54 +0900

    fix

--- packages/cdk/lib/construct/api.ts
+++ packages/cdk/lib/construct/api.ts
@@ -71,9 +71,16 @@ export class Api extends Construct {
     // Validate Model Names
     const supportedModelIds = [
       'anthropic.claude-3-5-sonnet-20240620-v1:0',
+      'us.anthropic.claude-3-5-sonnet-20240620-v1:0',
+      'eu.anthropic.claude-3-5-sonnet-20240620-v1:0',
       'anthropic.claude-3-opus-20240229-v1:0',
+      'us.anthropic.claude-3-opus-20240229-v1:0',
       'anthropic.claude-3-sonnet-20240229-v1:0',
+      'us.anthropic.claude-3-sonnet-20240229-v1:0',
+      'eu.anthropic.claude-3-sonnet-20240229-v1:0',
       'anthropic.claude-3-haiku-20240307-v1:0',
+      'us.anthropic.claude-3-haiku-20240307-v1:0',
+      'eu.anthropic.claude-3-haiku-20240307-v1:0',
       'anthropic.claude-v2:1',
       'anthropic.claude-v2',
       'anthropic.claude-instant-v1',
@@ -100,6 +107,7 @@ export class Api extends Construct {
     ];
     const multiModalModelIds = [
       'anthropic.claude-3-5-sonnet-20240620-v1:0',
+      'us.anthropic.claude-3-5-sonnet-20240620-v1:0',
       'anthropic.claude-3-opus-20240229-v1:0',
       'anthropic.claude-3-sonnet-20240229-v1:0',
       'anthropic.claude-3-haiku-20240307-v1:0',
