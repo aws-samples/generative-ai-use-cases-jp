682e485edc2d0ab709801577a9a53d374ca9e7c8
Yusuke Wada <wadaysk@amazon.co.jp>
2024-11-18 10:46:34 +0900

    fix drawer

--- packages/web/src/components/useCaseBuilder/CustomUseCaseDrawerItems.tsx
+++ packages/web/src/components/useCaseBuilder/CustomUseCaseDrawerItems.tsx
@@ -2,6 +2,7 @@ import { UseCaseAsOutput } from 'generative-ai-use-cases-jp';
 import React, { useCallback } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { ROUTE_INDEX_USE_CASE_BUILDER } from '../../main';
+import useDrawer from '../../hooks/useDrawer';
 
 type Props = {
   useCases: UseCaseAsOutput[];
@@ -11,12 +12,14 @@ const CustomUseCaseDrawerItems: React.FC<Props> = (props) => {
   const { useCaseId } = useParams();
   const navigate = useNavigate();
   const location = useLocation();
+  const { switchOpen } = useDrawer();
 
   const onClick = useCallback(
     (useCaseId: string) => {
       navigate(`${ROUTE_INDEX_USE_CASE_BUILDER}/execute/${useCaseId}`);
+      switchOpen();
     },
-    [navigate]
+    [navigate, switchOpen]
   );
 
   return (
