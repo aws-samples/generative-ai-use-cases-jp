31d4b5cf48cc74c700da16ba3eed006725b7ceae
kazuu-bp <ksponpon.0215@gmail.com>
2025-02-22 10:25:43 +0900

    フロントエンドの環境変数に KnowledgeBase の ID を渡そうとしていたので削除

--- packages/cdk/lib/construct/web.ts
+++ packages/cdk/lib/construct/web.ts
@@ -20,7 +20,6 @@ export interface WebProps {
   predictStreamFunctionArn: string;
   ragEnabled: boolean;
   ragKnowledgeBaseEnabled: boolean;
-  ragKnowledgeBaseId?: string;
   agentEnabled: boolean;
   flows?: Flow[];
   flowStreamFunctionArn: string;
@@ -172,8 +171,6 @@ export class Web extends Construct {
         VITE_APP_PREDICT_STREAM_FUNCTION_ARN: props.predictStreamFunctionArn,
         VITE_APP_RAG_ENABLED: props.ragEnabled.toString(),
         VITE_APP_RAG_KNOWLEDGE_BASE_ENABLED:
-          props.ragKnowledgeBaseEnabled.toString(),
-        VITE_APP_RAG_KNOWLEDGE_BASE_ID:
           props.ragKnowledgeBaseEnabled.toString(),
         VITE_APP_AGENT_ENABLED: props.agentEnabled.toString(),
         VITE_APP_FLOWS: JSON.stringify(props.flows || []),
