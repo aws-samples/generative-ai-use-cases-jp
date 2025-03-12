e45a578f8f1018d53fe9ca7a09444de1b0505309
shintaok <shintaok@amazon.co.jp>
2025-03-12 06:36:50 +0000

    add region argument to bedrockApi

--- packages/types/src/utils.d.ts
+++ packages/types/src/utils.d.ts
@@ -7,20 +7,23 @@ import {
 export type InvokeInterface = (
   model: Model,
   messages: UnrecordedMessage[],
-  id: string
+  id: string,
+  region?: string
 ) => Promise<string>;
 
 export type InvokeStreamInterface = (
   model: Model,
   messages: UnrecordedMessage[],
   id: string,
-  idToken?: string
+  idToken?: string,
+  region?: string
 ) => AsyncIterable<string>;
 
 // Base64 にエンコードした画像を Return する
 export type GenerateImageInterface = (
   model: Model,
-  params: GenerateImageParams
+  params: GenerateImageParams,
+  region?: string
 ) => Promise<string>;
 
 export type ApiInterface = {
