16b5c7e4a41516b50845db78d3fd9426bb667117
Taichiro Suzuki <taichiro0709@gmail.com>
2025-03-27 08:32:23 +0900

    Add workflow to automate versioning (#962)
    Co-authored-by: Taichiro Suzuki <taichirs@amazon.co.jp>

--- packages/web/src/hooks/useVersion.ts
+++ packages/web/src/hooks/useVersion.ts
@@ -30,7 +30,8 @@ const useRemoteVersion = () => {
       if (!packageJson) {
         return false;
       } else {
-        return version !== packageJson.data.version;
+        // Compare only major versions
+        return version.split('.')[0] !== packageJson.data.version.split('.')[0];
       }
     },
   };
