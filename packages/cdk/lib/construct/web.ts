71b986af91a71fe6a5dd4a4325550fc733326b13
Yusuke Wada <wadaysk@amazon.co.jp>
2026-07-03 16:48:38 +0900

    fix(cdk): remove 'unsafe-eval' from CSP script-src directive

--- packages/cdk/lib/construct/web.ts
+++ packages/cdk/lib/construct/web.ts
@@ -99,7 +99,7 @@ export class Web extends Construct {
       const cspSaml = props.samlCognitoDomainName
         ? ` https://${props.samlCognitoDomainName}`
         : '';
-      const csp = `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; media-src 'self' blob: https://*.amazonaws.com; connect-src 'self' https://*.amazonaws.com https://*.amazoncognito.com wss://*.amazonaws.com:* https://*.on.aws https://raw.githubusercontent.com https://api.github.com${cspSaml}; font-src 'self' https://fonts.gstatic.com data:; object-src 'none'; frame-ancestors 'none'; frame-src 'self' https://www.youtube.com/;`;
+      const csp = `default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; media-src 'self' blob: https://*.amazonaws.com; connect-src 'self' https://*.amazonaws.com https://*.amazoncognito.com wss://*.amazonaws.com:* https://*.on.aws https://raw.githubusercontent.com https://api.github.com${cspSaml}; font-src 'self' https://fonts.gstatic.com data:; object-src 'none'; frame-ancestors 'none'; frame-src 'self' https://www.youtube.com/;`;
 
       // Create Response Headers Policy for security headers
       const responseHeadersPolicy = new ResponseHeadersPolicy(
